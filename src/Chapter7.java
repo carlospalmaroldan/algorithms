de21b0ffd087206afb69f304c6ed9d70e97619e5
carlos.palma <carlos.x.palma-nd@abc.com>
2019-06-12 09:43:36 -0500

    quicksort decrease implementation

--- src/Chapter7.java
+++ src/Chapter7.java
@@ -3,6 +3,10 @@ public class Chapter7 {
         int[] A={13, 19, 9, 5, 12, 8, 7, 4, 21, 2, 6, 11};
         quicksort(A,0,A.length-1);
         printArray(A);
+        int[] B={13, 19, 9, 5, 12, 8, 7, 4, 21, 2, 6, 11};
+        quicksortDecrease(B,0,B.length-1);
+        System.out.println("Decreasing");
+        printArray(B);
     }
 
     public static int partition(int[] A,int p, int r){
@@ -29,6 +33,33 @@ public class Chapter7 {
             int q=partition(A,p,r);
             quicksort(A,p,q-1);
             quicksort(A,q+1,r);
+        }
+    }
+
+    public static  int partitionDecrease(int[] A,int p, int r){
+        int x=A[r];
+        int i=p-1;
+        int temp=0;
+        for(int j=p;j<r;j++){
+            if(A[j]>x){
+                i=i+1;
+                temp=A[i];
+                A[i]=A[j];
+                A[j]=temp;
+
+            }
+        }
+        temp=A[i+1];
+        A[i+1]=A[r];
+        A[r]=temp;
+        return i+1;
+    }
+
+    public static void quicksortDecrease(int[] A,int p,int r){
+        if(p<r){
+            int q=partitionDecrease(A,p,r);
+            quicksortDecrease(A,p,q-1);
+            quicksortDecrease(A,q+1,r);
         }
     }
 
