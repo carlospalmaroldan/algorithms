a00e83fba94b354f78aff6df4c98cb5ec9ed5934
palma <carlospalma03@gmail.com>
2019-06-25 15:49:27 -0500

    hoare partition quicksort

--- src/Chapter7.java
+++ src/Chapter7.java
@@ -7,6 +7,10 @@ public class Chapter7 {
         quicksortDecrease(B,0,B.length-1);
         System.out.println("Decreasing");
         printArray(B);
+        int[] C={13, 19, 9, 5, 12, 8, 7, 4, 11, 2, 6, 21};
+        quicksortHoare(C,0,C.length-1);
+        System.out.println("Hoare");
+        printArray(C);
     }
 
     public static int partition(int[] A,int p, int r){
@@ -33,6 +37,14 @@ public class Chapter7 {
             int q=partition(A,p,r);
             quicksort(A,p,q-1);
             quicksort(A,q+1,r);
+        }
+    }
+
+    public static void quicksortHoare(int[] A,int p,int r){
+        if(p<r){
+            int q=hoarePartition(A,p,r);
+            quicksortHoare(A,p,q);
+            quicksortHoare(A,q+1,r);
         }
     }
 
@@ -66,6 +78,28 @@ public class Chapter7 {
     public static void printArray(int[] A){
         for(int j=0;j<A.length;j++) {
             System.out.println(A[j]);
+        }
+    }
+
+    public static int hoarePartition(int[] A,int p,int r){
+        int x=A[p];
+        int i=p-1;
+        int j=r+1;
+        while(true){
+            do{
+                j=j-1;
+            }while(A[j]>x);
+            do{
+                i=i+1;
+            }while(A[i]<x);
+
+            if (i<j){
+                int temp= A[i];
+                A[i]=A[j];
+                A[j]=temp;
+            }else{
+                return j;
+            }
         }
     }
 }
