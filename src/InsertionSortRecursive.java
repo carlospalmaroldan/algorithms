ebb00b78b765ec16c2918a792468bc4109272617
carlospalma <carlospalma03@gmail.com>
2019-12-09 10:47:33 -0500

    exercise 2.3-4 Cormen insertion sort recursive

---
+++ src/InsertionSortRecursive.java
@@ -0,0 +1,32 @@
+public class InsertionSortRecursive {
+
+
+    public static void main(String[] args){
+
+        int[] A={2,1,5,0};
+        insertionSortRecursive(A,3);
+        for(int i=0;i<A.length;i++) {
+            System.out.println(A[i]);
+        }
+    }
+
+    public static void insertionSortRecursive(int[] A, int index){
+        if(index>0){
+            insertionSortRecursive(A, index-1);
+            insert(A,index);
+        }else {
+            insert(A, index);
+        }
+    }
+
+    public static void insert(int[] A,int j) {
+        if (j < A.length - 1) {
+            int k = A[j + 1];
+            while (j > -1 && A[j] > k) {
+                A[j + 1] = A[j];
+                j = j - 1;
+            }
+            A[j + 1] = k;
+        }
+    }
+}
