c32cb48c9ac83e0b8740da45b912d6d2dcbe838d
carlospalma <carlospalma03@gmail.com>
2019-12-10 08:40:55 -0500

    cormen exercise 2.3-5

---
+++ src/BinarySearch.java
@@ -0,0 +1,34 @@
+import static java.lang.Math.ceil;
+
+public class BinarySearch {
+
+
+    public static void main(String[] args){
+        int[] A={1,3,4,7,9,12};
+        System.out.println(binarySearch(A,0,A.length-1,12));
+        System.out.println(binarySearch(A,0, A.length-1,8));
+    }
+
+    public static Double binarySearch(int[] A,double start, double end, int v){
+        if(end-start>1){
+            double mid=ceil((end-start)/2) + start;
+            if (A[(int)mid] > v){
+                return binarySearch(A,start, mid, v);
+            }else if(A[(int)mid] < v){
+                return binarySearch(A,mid,end,v);
+            }else if(A[(int)mid]== v){
+                return mid;
+            }
+        } else {
+            if(A[(int)start]==v){
+                return Double.valueOf(start);
+            }else if(A[(int)end]==v){
+                return Double.valueOf(end);
+            }
+            else return null;
+        }
+        return null;
+    }
+
+
+}
